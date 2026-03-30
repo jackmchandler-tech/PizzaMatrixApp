d821922ef1dc94e437fd4efa5defac573835cdcf
jackmchandler-tech <jack.m.chandler@gmail.com>
2026-03-30 10:32:37 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useState, useMemo} from "react";
 import { AppStateProvider, useAppState } from "./context/AppState";
 import { buildCoverageSummary, summarizePlanRow } from "./utils/calculations";
 import { PrintView } from "./components/PrintView";
