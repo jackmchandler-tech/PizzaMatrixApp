82e7c45a278520c5e278550803a66ce0e53c4a77
jackmchandler-tech <jack.m.chandler@gmail.com>
2026-03-30 17:47:29 -0400

    Update PrintView.tsx

--- src/components/PrintView.tsx
+++ src/components/PrintView.tsx
@@ -112,33 +112,30 @@ export function PrintView({ data }: { data: AppData }) {
       <section style={styles.section}>
         <h1 style={styles.title}>Pizza Plan</h1>
 
-            <table style={{ ...styles.table, marginBottom: "12px" }}>
-              <tbody>
-                <tr>
-                  {/* Date */}
-                  <td style={{ ...styles.td, width: "20%", fontWeight: 600 }}>
-                    {data.activeParty.date ?? ""}
-                  </td>
-            
-                  {/* Diners */}
-                  <td style={{ ...styles.td, width: "15%", fontWeight: 600 }}>
-                    {data.activeParty.diners} diners
-                  </td>
-            
-                  {/* Summary */}
-                  <td style={{ ...styles.td, width: "65%", fontWeight: 600 }}>
-                    {planRows.reduce((sum, r) => sum + r.row.quantity, 0)} pizzas serving{" "}
-                    {Math.round(summary.plannedServings * 100) / 100}
-                    {summary.needsMore
-                      ? ` (short ${Math.abs(Math.round(summary.delta * 100) / 100)})`
-                      : ""}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-        
-          </div>
-        </div>
+          <table style={{ ...styles.table, marginBottom: "12px" }}>
+            <tbody>
+              <tr>
+                {/* Date */}
+                <td style={{ ...styles.td, width: "20%", fontWeight: 600 }}>
+                  {data.activeParty.date ?? ""}
+                </td>
+          
+                {/* Diners */}
+                <td style={{ ...styles.td, width: "15%", fontWeight: 600 }}>
+                  {data.activeParty.diners} diners
+                </td>
+          
+                {/* Summary */}
+                <td style={{ ...styles.td, width: "65%", fontWeight: 600 }}>
+                  {planRows.reduce((sum, r) => sum + r.row.quantity, 0)} pizzas serving{" "}
+                  {Math.round(summary.plannedServings * 100) / 100}
+                  {summary.needsMore
+                    ? ` (short ${Math.abs(Math.round(summary.delta * 100) / 100)})`
+                    : ""}
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
         <table style={styles.table}>
           <thead>
