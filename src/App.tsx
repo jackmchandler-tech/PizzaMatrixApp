5616e1bb3b6995714f16deabad5f8e1ec0d58648
jackmchandler-tech <jack.m.chandler@gmail.com>
2026-04-07 11:05:36 -0400

    Update App.tsx
    Added component for ReleaseNotes.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { PartyHistory } from "./components/PartyHistory";
 import type { PizzaRecipe } from "./types";
 import { APP_VERSION } from "./constants";
 import { InstructionManual } from "./components/InstructionManual";
+import { ReleaseNotes } from "./components/ReleaseNotes";
 
 function PlannerScreen() {
   const {
